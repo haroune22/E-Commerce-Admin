59f1d00068af3770e4876ddf10ccaafcaff71ecd
haroune22 <harounehamenniche123@gmail.com>
2023-12-08 12:00:19 +0100

    fixed color value

--- app/(dashboard)/[storeId]/(routes)/colors/_components/Columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/_components/Columns.tsx
@@ -23,7 +23,7 @@ export const columns: ColumnDef<ColorColumn>[] = [
     cell:({row})=>{
       <div className="flex items-center gap-x-2">
         {row.original.value}
-        <div className="h-6 w-6 rounded-full border" style={{backgroundColor:row.original.value}}/>
+        <div className="h-6 w-6 rounded-full border" style={{ backgroundColor: row.original.value }} />
       </div>
     }
   },
